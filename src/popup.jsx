23b1f2f9659a1a3956d8dc9b58930145e2b101cf
Milan Gurung <milan.gurungnalim@gmail.com>
2022-06-16 12:30:56 +0545

    Initial Commit

---
+++ src/popup.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return <div>Hello Moegle</div>;
+};
+
+render(<App />, document.getElementById("root"));
